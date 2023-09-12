ba65220f7cdbe317da8cb52a6c7768ebf95e0527
Kuznetsova Elena <kuzn.elena@yahoo.com>
2023-09-12 13:08:12 +0500

    add basic game logic

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,40 +1,108 @@
 import { useEffect, useState } from "react";
 import createBoard from "../util/createBoard";
+import { revealed } from "../util/reveal";
 import Cell from "./Cell";
+import Timer from "./Timer";
 
 const Board = () => {
     const [grid, setGrid] = useState([]);
+    const [nonMineCount, setNonMineCount] = useState(0);
+    const [mineLocation, setMineLocations] = useState([]);
+    const [gameOver, setGameOver] = useState(false);
 
+
+    // ComponentDidMount
     useEffect(() => {
-        function freshBoard() {
-            const newBoard = createBoard(5, 5, 10);
-            setGrid(newBoard.board);
-        }
+        // Creating a board
+        // function freshBoard() {
+        //     const newBoard = createBoard(10, 10, 15);
+        //     setNonMineCount(10*10 - 15);
+        //     setMineLocations(newBoard.mineLocation);
+        //     setGrid(newBoard.board);
+        // }
+
+        // Calling a function
         freshBoard();
     }, []);
 
+    const freshBoard = () => {
+        const newBoard = createBoard(10, 15, 15);
+        setNonMineCount(10 * 15 - 15);
+        setMineLocations(newBoard.mineLocation);
+        setGrid(newBoard.board);
+    };
+
+
+    // On Right click / Flag Cell
     const updateFlag = (e, x, y) => {
+        // to not have dropdown on right click
         e.preventDefault();
+
+        // Deep copy of state
         let newGrid = JSON.parse(JSON.stringify(grid));
         console.log(newGrid[x][y])
         newGrid[x][y].flagged = true;
         setGrid(newGrid);
-        console.log('Right click');
+    };
+
+    // Reveal Cell
+    const revealCell = (x, y) => {
+        if (grid[x][y].revealed || gameOver) {
+            return;
+        }
+        let newGrid = JSON.parse(JSON.stringify(grid));
+        if (newGrid[x][y].value === 'X') {
+            alert('Mine found');
+            for (let i = 0; i < mineLocation.length; i++) {
+                newGrid[mineLocation[i][0]][mineLocation[i][1]].revealed = true;
+            }
+            setGrid(newGrid);
+            setGameOver(true);
+        } else {
+            let newRevealedBoard = revealed(newGrid, x, y, nonMineCount);
+            // newGrid[x][y].revealed = true;
+            setGrid(newRevealedBoard.arr);
+            setNonMineCount(newRevealedBoard.newNonMinesCount);
+            if (newRevealedBoard.newNonMinesCount === 0) {
+                setGameOver(true);
+            }
+        }
     }
 
-    // if (!grid.board) {
-    //     return <div>Loading</div>
-    // }
-
-    return grid.map(singleRow=>{
-        return (
-            <div style={{display: "flex"}}>
-            { singleRow.map(singleBlock => {
-            return <Cell details={singleBlock} updateFlag={updateFlag} />
-            })} 
+    return (
+        <div>
+            <p>{JSON.stringify(gameOver)}</p>
+            <Timer />
+            {/* <p>{nonMineCount}</p> */}
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                }}>
+                {grid.map(singleRow=>{
+                return (
+                <div style={{display: "flex"}}>
+                    {singleRow.map(singleBlock => {
+                    return (
+                        <Cell 
+                            revealCell={revealCell} 
+                            details={singleBlock} 
+                            updateFlag={updateFlag} 
+                        />
+                        );
+                    })} 
+                </div>
+                ) 
+                })}
             </div>
-        ) 
-    });
+            <button 
+                onClick={freshBoard}
+                className="restartBtn"
+            >
+            Restart game
+            </button> 
+        </div>
+    ); 
 };
 
 export default Board;
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,21 +1,79 @@
-export default function Cell({details, updateFlag}) {
+import { mineColor } from "../util/mineColors";
+import Circle from "./Circle";
+
+export default function Cell({details, updateFlag, revealCell}) {
+    const cellstyle = {
+        background: details.revealed
+          ? details.value === "X"
+            ? mineColor()
+            : bombChexPattern(details.x, details.y)
+          : chexPattern(details.x, details.y),
+        color: numColorCode(details.value),
+    };
     return (
         <div 
             onContextMenu={(e)=> updateFlag(e, details.x, details.y)} 
-            onClick={()=> console.log(details)} 
-            style={style.cellStyle}>
-            {details.reveal ? details.value : ""}
+            onClick={()=> revealCell(details.x, details.y)} 
+            style={cellstyle}
+            className = "cellStyle"
+            >
+            
+            {!details.revealed && details.flagged 
+            ? '🚩' 
+            : details.revealed && details.value !== 0 
+            ? (details.value === 'X' 
+                ? <Circle /> 
+                : details.value) 
+                : ('')}            
+            {/* {details.revealed ? details.value : ""} */}
+            {/* {details.value !== 0 && details.value} */}
         </div>
     )
 }
 
-const style = {
-    cellStyle: {
-        width: 40, 
-        height: 40, 
-        background: "grey",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center"
+
+const bombChexPattern = (x, y) => {
+    if (x % 2 === 0 && y % 2 === 0) {
+        return "#e5c29f";
+    } else if (x % 2 === 0 && y % 2 !== 0) {
+        return "#d7b899";
+    } else if (x % 2 !== 0 && y % 2 === 0) {
+        return "#d7b899";
+    } else {
+        return "#e5c29f";
     }
-}
+};
+
+const chexPattern = (x, y) => {
+    if (x % 2 === 0 && y % 2 === 0) {
+        return "#aad751";
+    } else if (x % 2 === 0 && y % 2 !== 0) {
+        return "#a2d249";
+    } else if (x % 2 !== 0 && y % 2 === 0) {
+        return "#a2d249";
+    } else {
+        return "#aad751";
+    }
+};
+
+const numColorCode = (num) => {
+    if (num === 1) {
+        return "#1976d2";
+    } else if (num === 2) {
+        return "#388d3c";
+    } else if (num === 3) {
+        return "#d33030";
+    } else if (num === 4) {
+        return "#0b1275";
+    } else if (num === 5) {
+        return "#964b00";
+    } else if (num === 6) {
+        return "#30d5c8";
+    } else if (num === 7) {
+        return "#000000";
+    } else if (num === 8) {
+        return "#ffffff";
+    } else {
+        return "grey";
+    }
+};
